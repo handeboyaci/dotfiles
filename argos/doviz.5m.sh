65752d8549135953d66e0a1819db068c10740025
Sencer Selcuk <sencerselcuk@gmail.com>
2019-11-26 14:41:22 -0500

    tbd

--- argos/doviz.5m.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-url="https://forex.1forge.com/1.0.3/quotes?pairs=USDTRY,EURTRY,EURUSD&api_key" 
-data="$(curl -s "$url=$(pass 1forge)" | jq '.[]|[.symbol,.price]|@sh')"
-
-echo ${data} | awk 'NR==1{printf("%.3f\n---\n", $2);exit}'
-echo "${data}"|tr -d \'|tr -d \"

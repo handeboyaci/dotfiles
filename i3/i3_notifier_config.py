a50d87797ed9c414908cb4e7a49c26931f3b486b
Sencer Selcuk <sencerselcuk@gmail.com>
2020-08-08 13:47:44 -0400

    i3-notifier & some minor updates

---
+++ i3/i3_notifier_config.py
@@ -0,0 +1,67 @@
+import subprocess
+import time
+
+from i3notifier.config import Config
+from i3notifier.utils import RunAsyncFactory
+
+
+class DefaultConfig(Config):
+    pre_action_hooks = [
+        # Start a script to listen for urgent workspaces & switch to it
+        RunAsyncFactory(lambda n: subprocess.call("switch-to-urgent.py")),
+        # Wait for the script become available
+        lambda n: time.sleep(0.2),
+    ]
+
+
+def ChromeAppFactory(title, url, icon=None, second_key="body"):
+    kChrome = "Google Chrome"
+    lURL = len(url)
+    icon = icon or "chrome"
+
+    class ChromeApp(DefaultConfig):
+        def should_apply(notification):
+            return (
+                notification.body.startswith(url) and notification.app_name == kChrome
+            )
+
+        def update_notification(notification):
+            notification.body = notification.body[lURL:].strip()
+            notification.app_name = title
+            notification.app_icon = icon
+
+        def get_keys(notification):
+            return title, str(getattr(notification, second_key))
+
+    return ChromeApp
+
+
+class NotifySend(DefaultConfig):
+    expires = True
+
+    def should_apply(notification):
+        return notification.app_name == "notify-send"
+
+
+Gmail = ChromeAppFactory("Gmail", "mail.google.com", "gmail")
+Gmail.pre_close_hooks = ["ignore"]
+Gmail.post_close_hooks = ["ignore"]
+
+Chat = ChromeAppFactory("Chat", "chat.google.com", "google-chat")
+Chat.pre_close_hooks = ["ignore"]
+Chat.post_close_hooks = ["ignore"]
+
+Meet = ChromeAppFactory("Meet", "meet.google.com", "meet")
+Meet.pre_close_hooks = ["ignore"]
+Meet.post_close_hooks = ["ignore"]
+
+config_list = [
+    Gmail,
+    Chat,
+    Meet,
+    ChromeAppFactory("WhatsApp", "web.whatsapp.com", "web-whatsapp", "summary"),
+    ChromeAppFactory("Twitter", "twitter.com", "twitter"),
+    ChromeAppFactory("Instagram", "www.instagram.com", "photos"),
+    NotifySend,
+    DefaultConfig,
+]

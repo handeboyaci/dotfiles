9728ef68f4d8e4deca802d1c13bc9f861f08ca52
Sencer Selcuk <sencerselcuk@gmail.com>
2020-10-29 16:09:57 -0400

    rofi themes

---
+++ bin/mute-on-lock.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+[[ $(pgrep mute-on-lock.sh | wc -l) -gt 1 ]] && exit 0
+
+gdbus monitor -y -d org.freedesktop.login1 | \
+  command grep -oP "(?<='LockedHint': <)[^>]*" --line-buffered | \
+    while read line
+    do
+      pactl set-sink-mute @DEFAULT_SINK@ $line
+    done

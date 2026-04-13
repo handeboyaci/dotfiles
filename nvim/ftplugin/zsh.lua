2e646b0fc06b9ad2b60b76590b6360a24625dd99
Sencer Selcuk <sencerselcuk@gmail.com>
2026-04-13 10:14:29 -0400

    update some submodules

--- nvim/ftplugin/zsh.lua
+++ nvim/ftplugin/zsh.lua
@@ -1,1 +1,22 @@
-sh.lua
+local null_ls = require("null-ls")
+local path = os.getenv("HOME") .. "/.local/share/nvim/mason/bin/"
+null_ls.setup({
+	sources = {
+		null_ls.builtins.formatting.shfmt.with({
+			command = path .. "shfmt",
+			extra_args = {
+				"--indent",
+				2,
+			},
+		}),
+		null_ls.builtins.formatting.shellharden.with({
+			command = path .. "shellharden",
+		}),
+	},
+})
+
+vim.lsp.start({
+	name = "Bash-LSP",
+	cmd = { path .. "bash-language-server", "start" },
+	single_file_support = true,
+})

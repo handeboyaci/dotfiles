39a66eafaa7f0b362ce2ac8b7929ca57ecc7b008
Sencer Selcuk <sencerselcuk@gmail.com>
2026-04-23 15:07:37 -0400

    delete mutt settings

--- mail/imap-pass.py
+++
@@ -1,8 +0,0 @@
-from sys import argv
-from subprocess import check_output
-
-def get_pass(arg):
-    return check_output(['pass', arg]).split()[0]
-
-if __name__ == '__main__':
-    print(get_pass(argv[1]))

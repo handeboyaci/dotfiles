7a5c9b52ffccfe21574bffae87e858328593e688
Sencer Selcuk <sencerselcuk@gmail.com>
2026-04-28 09:56:23 -0400

    Add modular custom options support to tmux-selector and update zshrc

--- zsh/tmux-selector/tmux-selector.zsh
+++ zsh/tmux-selector/tmux-selector.zsh
@@ -1,6 +1,22 @@
 [[ -n $TMUX ]] && return
 
 function _tmux_selector() {
+  local -A custom_actions
+  while [[ $# -gt 0 ]]; do
+    case "$1" in
+      --extra)
+        local item="$2"
+        local name="${item%%|*}"
+        local action="${item#*|}"
+        custom_actions[$name]="$action"
+        shift 2
+        ;;
+      *)
+        shift
+        ;;
+    esac
+  done
+
   local get_session_format () {
     local sep='},#{'
     local session_format=(session_name
@@ -25,28 +41,42 @@ function _tmux_selector() {
 
   local run_selector () {
     (
-      echo "Create new session"
+      echo "> Create new session"
       for session in $tmux_sessions
       do
         format_tmux_session $session
       done
-     ) | fzf | cut -d' ' -f 2
+      for name in ${(k)custom_actions}; do
+        echo "> $name"
+      done
+     ) | fzf
   }
 
   local tmux_sessions=($(tmx2 list-sessions -F $_session_format 2>/dev/null))
 
-  [[ ${#tmux_sessions} -eq 0 ]] && exec tmx2
+  [[ ${#tmux_sessions} -eq 0 && ${#custom_actions} -eq 0 ]] && exec tmx2
 
   local choice=$(run_selector)
+  [[ -z $choice ]] && return
 
-  if [[ $choice == "new" ]]
+  local type=$(echo $choice | cut -d' ' -f 1)
+
+  if [[ $type == ">" ]]
   then
-    exec tmx2 new
-  elif [[ ! -z $choice ]]
+    local name="${choice#* }"
+    if [[ $name == "Create new session" ]]
+    then
+      exec tmx2 new
+    elif [[ -n $custom_actions[$name] ]]
+    then
+      eval $custom_actions[$name]
+    fi
+  elif [[ $type == "+" || $type == "-" ]]
   then
-    exec tmx2 attach -t $choice
+    local val=$(echo $choice | cut -d' ' -f 2)
+    exec tmx2 attach -t $val
   fi
 
 }
 
-_tmux_selector
+_tmux_selector "$@"
